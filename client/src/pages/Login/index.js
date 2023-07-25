86d17b461e690eacf18943efa1689207d38ebab9
Phillip Lisun <l.phillip@wustl.edu>
2023-07-26 00:28:27 +1200

    login form redone

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import Alert from 'react-bootstrap/Alert';
+import React, {Component } from 'react';
 import WelcomeNav from '../../components/welcomeNavBar';
 import LoginForm from './loginForm.js';
 import './style.css';
+
 
 
 
@@ -14,19 +13,8 @@ class Login extends Component {
         return (
             <div className="App" id="loginPage">
                 <WelcomeNav></WelcomeNav>
-                <br />
-                <div id='alertDiv'>
-                    <Alert id="fieldAlert" variant="danger">Missing Field: </Alert>
-                </div>
-
                 <div className="body" id="login">
-                    <div className="loginFormDiv">
-                        <h2 id="loginHead">Login</h2>
-                            <LoginForm />
-                            <Button variant="primary" type="button" size="lg" id="loginButton" onClick={login}>
-                                    Login
-                            </Button>
-                    </div>
+                    <LoginForm />
                 </div>
             </div>
 
@@ -37,68 +25,5 @@ class Login extends Component {
     }
 }
 
-function login() {
-    let fieldAlert = "";
-    document.getElementById("fieldAlert").style.display = "none";
-    let check = true;
-
-    let email = document.getElementById("formGridEmail").value + "@wustl.edu";
-    let pwd = document.getElementById("password").value + "";
-
-    if(email === "@wustl.edu") {
-        fieldAlert = fieldAlert + "Email ";
-    }
-    if(pwd === "") {
-        fieldAlert = fieldAlert + "Password ";
-    }
-    if(fieldAlert !== "") {
-        addAlert(fieldAlert);
-    }
-
-    function addAlert(field) {
-        check = false;
-        document.getElementById("fieldAlert").innerText = "Missing Field: ";
-        document.getElementById("fieldAlert").innerText += field;
-        document.getElementById("fieldAlert").style.display = "block";
-    }
-
-    if(check === true) {
-        const data = {"email": email, "pwd": pwd};
-        sendLogin(data);
-    }  
-
-}
-
-async function sendLogin(data) {
-    try {
-        const response = await fetch("/api/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
-
-        const result = await response.json();
-        console.log("Success:", result);
-
-        if(result.success === "true") {
-            sessionStorage.setItem("email", data.email);
-            sessionStorage.setItem("token", result.token);
-            window.location.href = '/marketplace'
-        }
-        if(result.success === false) {
-            document.getElementById("fieldAlert").innerText = "Incorrect Username or Password";
-            document.getElementById("fieldAlert").style.display = "block";
-            
-        }
-
-        } catch(error) {
-        console.error("Error:", error);
-
-    }
-
-
-}
 
 export default Login;
--- client/src/pages/Login/loginForm.js
+++ client/src/pages/Login/loginForm.js
@@ -1,24 +1,164 @@
-import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form';
+import React, {Component, useState} from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Fade from 'react-bootstrap/Fade';
+import Alert from 'react-bootstrap/Alert';
+import Row from 'react-bootstrap/Row';
+
+
+
+
 
 class LoginForm extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {open: false, validated: false};
+    }
+
     render() {
+        
+        const sendData = async() => {
+            if(await prepareData() === false) {
+                this.setState({open: true});
+            }
+        };
+
+        const handleSubmit = (event) => {
+            const form = event.currentTarget;
+
+            if(form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            if(form.checkValidity() === true)  {
+                sendData();
+            }
+        
+            this.setState({validated: true});
+            
+          };
+
         return(
-            <Form className="loginFormGrid">
-                <Form.Group class="mb-3" controlId="formGridEmail">
-                    <InputGroup>
-                        <Form.Control type="text" placeholder="Example.p" />
-                        <InputGroup.Text>@wustl.edu</InputGroup.Text>
-                    </InputGroup>
-                </Form.Group>
-                <Form.Group class="mb-3" controlId="password">
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-            </Form>
+
+            <div className="loginFormDiv">
+
+                <h2 id="loginHead">Login</h2>
+          
+
+                <Form className="loginFormGrid" noValidate validated={this.state.validated}>
+                    {/* <Row className="mb-3"> */}
+                    <div className="mb-3">
+
+                        <InputGroup>
+                            <FloatingLabel label="Email">
+                                <Form.Control id="formGridEmail" type="text" placeholder="" required/>
+                            </FloatingLabel>
+                            <InputGroup.Text>@wustl.edu</InputGroup.Text>
+                        </InputGroup>
+                    {/* </Row> */}
+                    </div>
+
+
+                    {/* <Row className="mb-3"> */}
+                    <div className="mb-3">
+
+                        <FloatingLabel label="Password">
+                            <Form.Control id="password" type="password" placeholder="" required />
+                        </FloatingLabel>
+                    {/* </Row> */}
+                    </div>
+
+                    <div className="mb-3">
+
+                    <Button variant="primary" type="button" onClick={handleSubmit} size="lg" id="loginButton">
+                        Login
+                    </Button>
+                    </div>
+
+
+                </Form>
+                
+
+                <Fade in={this.state.open}>
+                    <div>
+                        <Alert id="fieldAlert" variant="danger">Missing Field: </Alert>
+                    </div>                    
+                </Fade>
+
+            </div>
+
 
         );
     }
 }
+
+async function prepareData() {
+
+    let email = document.getElementById("formGridEmail").value + "@wustl.edu";
+    let pwd = document.getElementById("password").value + "";
+
+    if(email === "@wustl.edu") {
+        return;
+    }
+    if(pwd === "") {
+        return;
+    }
+    else {
+        if(await login() === false) {
+            return false;
+        }
+    }
+}
+
+async function login() {
+
+
+    let email = document.getElementById("formGridEmail").value + "@wustl.edu";
+    let pwd = document.getElementById("password").value + "";
+
+    const data = {"email": email, "pwd": pwd};
+    if(await sendLogin(data) === false) {
+        return false;
+    }
+
+
+
+}
+
+async function sendLogin(data) {
+    
+    try {
+        const response = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log("Success:", result);
+
+        if(result.success === "true") {
+            sessionStorage.setItem("email", data.email);
+            sessionStorage.setItem("token", result.token);
+            window.location.href = '/marketplace'
+        }
+        if(result.success === false) {
+            document.getElementById("fieldAlert").innerText = "Incorrect Username or Password";
+            return false;
+        }
+
+        } catch(error) {
+        console.error("Error:", error);
+
+    }
+
+
+}
+
 
 export default LoginForm;
